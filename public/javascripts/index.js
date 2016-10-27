98e5b16a5cdf0ecccc37d864d72eaa105faef688
iMARs57 <ar.chen57@gmail.com>
2016-10-27 12:58:15 +0800

    first commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,37 @@
+$(function() {
+    $('.circle_htmlcss').percentcircle({
+        fillColor: '#f16529',
+    });
+    $('.circle_js').percentcircle();
+    $('.circle_cpp').percentcircle({
+        fillColor: '#66f400',
+    });
+    $('.circle_php').percentcircle({
+        fillColor: '#A83EFF',
+    });
+    $('.circle_mysql').percentcircle({
+        fillColor: '#8bc0ff',
+    });
+    $('.circle_unix').percentcircle({
+        fillColor: '#ffa73f',
+    });
+    $('.circle_ror').percentcircle({
+        fillColor: '#ff0000',
+    });
+    $('.circle_node').percentcircle({
+        fillColor: '#8fc84e',
+    });
+    $('.circle_react').percentcircle({
+        fillColor: '#6ed0f2',
+    });
+    $('.circle_git').percentcircle({
+        fillColor: '#f03c2e',
+    });
+    $('.circle_java').percentcircle({
+        fillColor: '#da2b2a',
+    });
+    $('.circle_asp').percentcircle({
+        fillColor: '#3c3a3d',
+    });
+
+});
